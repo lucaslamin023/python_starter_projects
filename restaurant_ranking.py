a4c352d8708bb323412fbc73edb71374fc605f00
Lucas Lamin <99187859+lucaslamin023@users.noreply.github.com>
2025-03-05 12:44:06 -0300

    update restaurant_ranking

--- restaurant_ranking.py
+++ restaurant_ranking.py
@@ -30,19 +30,23 @@ def searchRestaurant(name):
 # função para editar um restaurante
 
 def newRate():
-    rate = float(input("A nova nota do restaurante é: "))
+    rate = float(input("Avaliação de 1 a 5 do restaurante: "))
+    while (rate < 1) or (rate > 5): # validando se a nota está nos parâmetros
+                rate = float(input("A avaliação deve estar entre 1 e 5! \nTente novamente: "))
     return rate
 def newDistance():
-    distance = float(input("A nova distancia do restaurante é: "))
+    distance = float(input("Distância em km do restaurante: "))
+    while (distance < 0): # validando se a distancia é maior que zero
+                distance = float(input("A distância deve ser maior que zero! \nTente novamente: "))
     return distance
-
 
 # Menu
 
 option = 0
 
 while option != 3:
-    print("| Escolha uma das opções: \n| 1 - Adicionar Restaurantes \n| 2 - Vizualizar Ranking \n| 3 - Sair ")
+    print("="*52)
+    print("| Escolha uma das opções: \n| 1 - Adicionar/Excluir Restaurantes \n| 2 - Vizualizar Ranking \n| 3 - Sair ")
     option = input("- ")
     if option in "123": # verificando se a variável option recebeu um valor válido
         option = int(option)
@@ -51,20 +55,32 @@ while option != 3:
     if option == 1:
         print("="*52)
         i = 1
-        answer = "S"
-        # criando um loop para adicionar novos restaurantes a partir de inputs
-        while answer == 'S':
-            name = input("Nome do {}° restaurante: ".format(i))
-            rate = float(input("Avaliação de 1 a 5 do restaurante {}: ".format(name)))
-            while (rate < 1) or (rate > 5): # validando se a nota está nos parâmetros
-                rate = float(input("A avaliação deve estar entre 1 e 5! \nTente novamente: "))
-            ds = float(input("Distância em km do restaurante {}: ".format(name)))
-            new_restaurant = [name, rate, ds] # criando uma lista com os inputs e alterando ela a cada loop
-            restaurants.append(new_restaurant)
-            answer = input("Deseja continuar a adicionar restaurantes? [S - Sim | N - Não]\n- ").upper()
-            i+=1
-            print("="*40)
-
+        add_or_remove = False
+        add_answer = 'S'
+        remove_answer = 'S'
+       
+        while add_or_remove != "3":
+            add_or_remove = input("| O que deseja fazer: | 1 - Adicionar | 2 - Excluir | 3 - Voltar\n- ")
+            if add_or_remove == "1":
+                while add_answer == 'S': # criando um loop para adicionar novos restaurantes a partir de inputs
+                    name = input("Nome do {}° restaurante: ".format(i))
+                    new_restaurant = [name, newRate(), newDistance()]
+                    restaurants.append(new_restaurant)
+                    add_answer = input("Deseja continuar a adicionar restaurantes? [S - Sim | N - Não]\n- ").upper()
+                    i+=1
+                    print("="*40)
+            elif add_or_remove == "2":
+                while remove_answer == 'S':
+                    search = input("| Digite 'Limpar' para remover todos os restaurantes | Ou o nome do restaurante que deseja remover. \n- ").upper()
+                    if search == "LIMPAR":
+                        restaurants.clear()
+                        answer = False
+                    else:
+                        removed_restaurant = searchRestaurant(search)
+                        if removed_restaurant == False:
+                            print("O restaurante não foi encotrado! Digite o nome do restaurante que deseja remover.")
+                        else:
+                            remove_answer = input("Deseja continuar a excluir restaurantes? [S - Sim | N - Não]\n- ").upper()
 
     # Vizualizar Ranking
     elif option == 2:
@@ -78,36 +94,34 @@ while option != 3:
             ranking.append(restaurant)
             #repete esse processo, obtendo novamente o restaurante com a maior nota na lista desordenada e o colocando na lista de raking
         
-        print("=-"*21)
+        print("=-"*27)
         print("|Rank| Restaurante | Nota | Distãncia")
         for restaurant in ranking: # loop para exibir cada elemento da lista ordenada
             print("| {}° | {} - {}★ - {}km".format(nRanking, restaurant[0], restaurant[1], restaurant[2]))
             nRanking += 1
-        print("=-"*21)
+        print("=-"*27)
         restaurants = ranking # retornando os elementos para lista restaurantes, para o caso do usuário escolher adicionar ou vizualizar os restaurantes novamente
 
         edit = input("Deseja editar algum restaurante? [S - Sim | N - Não]\n- ").upper()
-        print("-_"*38)
+        print("-_"*27)
 
     # Editar Restaurantes
         
         while edit == 'S':
             element = 1
-            search = input("Digite o nome do restaurante que deseja editar ")
+            search = input("Digite o nome do restaurante que deseja editar: ")
             edit_restaurant = searchRestaurant(search)
-
+            element = input("O que deseja alterar no restaurante {}?\n[1 - Nota | 2 - Distância | 3 - Ambos] - ". format(edit_restaurant[0]))
             if edit_restaurant == False:
                 print("O restaurante não foi encotrado! Digite o nome do restaurante que deseja editar.")
-            
             else:
-                elemento = input("O que deseja alterar no restaurante {}?\n[1 - Nota | 2 - Distância | 3 - Ambos] - ". format(edit_restaurant[0]))
                 if element == '1':
                     changed_restaurant = [edit_restaurant[0], newRate(), edit_restaurant[2]]
                 elif element == '2':
                     changed_restaurant = [edit_restaurant[0], edit_restaurant[1], newDistance()]
                 elif element == '3':
                     changed_restaurant = [edit_restaurant[0], newRate(), newDistance()]
-            
+                
                 restaurants.append(changed_restaurant)
 
             print("="*54)
